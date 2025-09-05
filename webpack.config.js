70edc566b3ae66612c65ef90ff954439f7055196
wista110 <wista.dev1@gmail.com>
2025-09-05 16:37:27 +0900

    プロジェクトの下準備

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path =require("path");
+const HtmlWebpackPlugin = repuire('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        })
+    ],
+    devServer: {
+        static: [
+            {
+                directory: path.join(__dirname, 'dist')
+            },
+            {
+                directory: path.join(__dirname, 'public'),
+                publicPath: '/'
+            }
+        ],
+        compress: true,
+        port: 3000        
+    }   
+};
