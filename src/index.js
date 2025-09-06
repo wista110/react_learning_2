acedeafa2692916d25ced2993e0d647e311ced41
wista110 <wista.dev1@gmail.com>
2025-09-06 23:30:37 +0900

    JSが表示されない問題を修正

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-const root = documentgetElementById('root');
+const root = document.getElementById('root');
 const rootElement = ReactDOM.createRoot(root);
 
 const h1 = React.createElement("h1", {}, "Hello World");
-const p =React.createElement("p", {}, "This is a paragraph");
+const p = React.createElement("p", {}, "This is a paragraph");
 const div = React.createElement("div", {}, [h1,p]);
 
 rootElement.render(div);
